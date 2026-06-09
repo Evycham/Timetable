9cea9273730a16134b145792207e4fc87ceb56fc
evycham <ygruzevich8484@gmail.com>
2026-06-09 17:43:29 +0200

    [BE-02] download

---
+++ app/src/main/java/com/example/timetable/data/DaVinciApi.kt
@@ -0,0 +1,24 @@
+package com.example.timetable.data
+
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
+
+class DaVinciApi {
+    private val url =
+        "https://infoserver.hochschule-stralsund.de/daVinciIS.dll?content=json"
+
+    fun download(): DaVinciResponse {
+        val jsonString = URL(url).readText()
+
+        val root = JSONObject(jsonString)
+
+        val lessonTimes = root.getJSONArray("lessonTimes")
+        val eventTimes = root.getJSONArray("eventTimes")
+
+        return DaVinciResponse(
+            lessonTimes = lessonTimes,
+            eventTimes = eventTimes
+        )
+    }
+}
